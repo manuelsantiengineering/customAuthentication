78d58fffa01d4741962c11d3d5a3de8050b9c6dd
Santiago Laboy <msanti04@harris.com>
2019-04-24 15:16:56 -0400

    Included all of the necessary java classes.

--- customAuthResource/src/test/java/com/msanti/spring/customAuthResource/AuthResourceApplicationTests.java
+++ customAuthResource/src/test/java/com/msanti/spring/customAuthResource/AuthResourceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomAuthResourceApplicationTests {
+public class AuthResourceApplicationTests {
 
 	@Test
 	public void contextLoads() {
